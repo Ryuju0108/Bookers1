22c1625239a6a117bb3ab626a23a271a5e2cb629
Ryuju <itou.ryuju.infratop@gmail.com>
2020-01-28 13:37:14 +0900

    Second commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,50 @@
 class BlogsController < ApplicationController
   def index
+  	@blogs = Blog.all
+  	@blog = Blog.new
   end
 
   def show
+  	@blog = Blog.find(params[:id])
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	blog = Blog.find(params[:id])
+  	if blog.update(blog_params)
+  		flash[:notice_update] = "Book was successfully updated."
+  		redirect_to blog_path(blog)
+  	else
+  		render("edit_blog_path(blog)")
+  	end
+  end
+
+  def new
+  end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	if blog.save
+  		flash[:create] = "Book was successfully created."
+  		redirect_to blogs_path
+  	else
+  		render("/blogs")
+  	end
+  end
+
+  def destroy
+  	blog = Blog.find(params[:id])
+  	if blog.destroy
+  		flash[:notice_destroy] = "Book was successfully destroyed."
+  		redirect_to blogs_path
+  	end
+  end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/edit'
+  resources :blogs
+  # delete '/blogs/:id', to: '/blogs#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
